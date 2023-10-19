b16d156f41584073f59e050dea8f0721343b9c91
Tahirahmad1002 <tahirahmad1002@gmail.com>
2023-10-19 14:32:18 -0700

    last

--- airdistance.c
+++ airdistance.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<math.h>
 int main(){
 
 double lat_ori, lon_ori, lat_des, lon_des;
@@ -32,17 +33,18 @@ double y = (lat_des*pi)/180;
 
 double z = (lon_des*pi)/180;
 
-double min = z-y;
+double min = z-x;
 
-double distance = acos( sin(w) * sin(x) + cos(w) * cos (x) * cos (min) * (radius));
+double distance = acos(sin(w) * sin(y) + cos(w) * cos(y) * cos(min)) * (radius);
 
-printf("\n\n\n ______LOCATION DISTANCE____\n\n");
+printf("\n\n\n Location Distance\n\n");
    
 printf(" Origin : ( %.6lf , %.6lf )\n\n",lat_ori,lon_ori);
 
 printf(" Destination : ( %.6lf , %.6lf )\n\n",lat_des,lon_des);
     
 printf("Air Distance : %.6lf KMS \n\n",distance);
+
 
 
 
