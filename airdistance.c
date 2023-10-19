a2baf77969cbcbe863101289ccc93f3ad03b3c57
Tahirahmad1002 <tahirahmad1002@gmail.com>
2023-10-19 14:04:06 -0700

    3rd

--- airdistance.c
+++ airdistance.c
@@ -1,8 +1,15 @@
 #include<stdio.h>
 int main(){
 
+double lat_ori, lon_ori, lat_des, lon_des;
 
+const float pi=3.14;
 
+const float radius= 6371.0;
+
+printf("\nEnter lon and lat of origin (in degrees)");
+printf("\nlon : ");
+scanf("%lf",&lat_ori);
 
 
 
